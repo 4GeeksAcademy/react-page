1330e66cffb0c63e402794f0270da68e92514e36
Martin Nusspaumer <mnusspaumer@gmail.com>
2023-10-24 17:03:34 -0400

    improvements

--- src/App.js
+++ src/App.js
@@ -5,16 +5,33 @@ import TheCard from './components/card';
 import TheFooter from './components/footer';
 
 function App() {
-  return (
+
+   return (
     <div className="App">
       <TheNavbar />
       <TheJumbotron />
       <div className='container flex justify-content-center'>
         <div className='row'>
-          <div className='col-12 col-sm-3'><TheCard /></div>
-          <div className='col-12 col-sm-3'><TheCard /></div>
-          <div className='col-12 col-sm-3'><TheCard /></div>
-          <div className='col-12 col-sm-3'><TheCard /></div>
+          <div className='col-12 col-sm-3'>
+            <TheCard
+              TheTitle='Card 1'
+              TheText='Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.'
+              TheImage="https://images.unsplash.com/photo-1618886487325-f665032b6352?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww" /></div>
+          <div className='col-12 col-sm-3'>
+            <TheCard
+             TheTitle='Card 2'
+             TheText='Duis aute irure dolor in Duis aute irure dolor in.'
+             TheImage="https://images.unsplash.com/photo-1650173901755-ad5526ac8b51?auto=format&fit=crop&q=80&w=3863&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" /></div>
+          <div className='col-12 col-sm-3'>
+            <TheCard
+             TheTitle='Card 3'
+             TheText='Excepteur sint occaecat cupidatat non proident'
+             TheImage="https://images.unsplash.com/photo-1608665488977-b26949036f6e?auto=format&fit=crop&q=80&w=3876&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" /></div>
+          <div className='col-12 col-sm-3'>
+            <TheCard
+             TheTitle='Card 4'
+             TheText='sunt in culpa qui officia deserunt mollit anim id est laborum.'
+             TheImage="https://images.unsplash.com/photo-1490682143684-14369e18dce8?auto=format&fit=crop&q=80&w=3870&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" /></div>
         </div>
       </div>
       <TheFooter />
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,18 +1,15 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function TheCard() {
+function TheCard(props) {
   return (
    
     <Card style={{ margin: '10px' }}>
-      <Card.Img style={{ height: '240px'}} variant="top" src="https://images.unsplash.com/photo-1618886487325-f665032b6352?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww" />
+      <Card.Img style={{ height: '240px'}} variant="top" src={props.TheImage} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Title>{props.TheTitle}</Card.Title>
+        <Card.Text>{props.TheText}</Card.Text>
+        <Button variant="primary">Find Out More!</Button>
       </Card.Body>
     </Card>
   
