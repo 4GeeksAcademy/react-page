f3c2e495128abf980784dddc6180ea4f0a899239
Martin Nusspaumer <mnusspaumer@gmail.com>
2023-10-24 17:37:29 -0400

    added functionality

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,19 +1,21 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function TheCard(props) {
+  const [textbtn, thanks] = useState('Find out more!')
   return (
-   
-    <Card style={{ margin: '10px' }}>
+    
+    <Card>
       <Card.Img style={{ height: '240px'}} variant="top" src={props.TheImage} />
       <Card.Body>
         <Card.Title>{props.TheTitle}</Card.Title>
         <Card.Text>{props.TheText}</Card.Text>
-        <Button variant="primary">Find Out More!</Button>
+        <Button onClick={() => thanks('You clicked!')} variant="primary">{textbtn}</Button>
       </Card.Body>
     </Card>
   
-  );
+  )
 }
 
 export default TheCard;
